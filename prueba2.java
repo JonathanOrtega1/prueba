bcfecd584b1770c7e74c7a7b2aa9164b5065f969
JonathanOrtega1 <jonathancamiloguacas@gmail.com>
2022-04-11 21:51:33 -0500

    version4

--- prueba2.java
+++ prueba2.java
@@ -2,5 +2,6 @@ public class prueba2 {
     public void name(String[] args) {
         System.out.println("Prueba 1");
         System.out.println("Prueba 2");
+        System.out.println("Prueba 3");
     }
 }
