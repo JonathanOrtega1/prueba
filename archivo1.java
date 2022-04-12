c8a7ab27ba19760fde65ac71dc23b1ca9772ac49
JonathanOrtega1 <jonathancamiloguacas@gmail.com>
2022-04-11 21:50:11 -0500

    version3

--- archivo1.java
+++ archivo1.java
@@ -1,5 +1,6 @@
 public class archivo1 {
     public static void main(String[] args) {
         System.out.println("Hoja 1");
+        System.out.println("Hoja 2");
     }
 }
--- prueba2.java
+++ prueba2.java
@@ -1,5 +1,6 @@
 public class prueba2 {
     public void name(String[] args) {
-        System.out.println("Hoja 2");
+        System.out.println("Prueba 1");
+        System.out.println("Prueba 2");
     }
 }
